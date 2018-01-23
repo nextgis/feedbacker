09623c3cc91a757ee19c87f759b5ad3d3efa4c5f
Anastasiya Lisovenko <lisovenko.ad@gmail.com>
2018-01-23 18:14:22 +0400

    lightbox gallery

--- src/components/DetailMessage.vue
+++ src/components/DetailMessage.vue
@@ -14,11 +14,16 @@
                 <div class="deatil-message__text">
                     {{message ? message.properties.text : "" }}
                 </div>
-                <template v-if="message && message.attachments">
-                    <div v-for="attachment in message.attachments"
+                <template v-if="message && message.attachments && message.attachments.length">
+                    <div v-for="attachment, id in message.attachments"
                          class="detail-message__photo">
-                        <img class="detail-message__pic" :src="attachment + '?size=400x800'" alt="">
+                        <img class="detail-message__pic" :src="attachment + '?size=400x800'" alt=""
+                             @click="galleryIndex = id">
                     </div>
+                    <vue-gallery :images="message.attachments"
+                                 :index="galleryIndex"
+                                 @close="galleryIndex = null"
+                                 ref="gallery"></vue-gallery>
                 </template>    
             </v-container>
         </v-card>
@@ -42,14 +47,17 @@
 import bus from "../js/eventBus"
 import Vue from "vue"
 import {mapState} from "vuex"
+import VueGallery from 'vue-gallery'
 
 export default {
+  components:{ VueGallery},
   props: [
     "message"
   ],
   data(){
     return {
-        isCardOverflowed: false
+        isCardOverflowed: false,
+        galleryIndex: null
     }
   },
   computed: {
@@ -60,9 +68,16 @@ export default {
   watch:{
     message(value){
         this.$el.scrollTop = 0;
-        if (value!=undefined) Vue.nextTick(()=>{ 
-            this.checkCardOverflowing();
-        })        
+        if (value!=undefined) {
+            Vue.nextTick(()=>{ 
+                this.checkCardOverflowing();
+            });
+
+            if (value.attachments && value.attachments.length)
+                Vue.nextTick(()=>{
+                    document.body.appendChild(this.$refs.gallery.$el);
+                });
+        }
     }
   },
   methods: {
@@ -134,17 +149,72 @@ export default {
         margin-top: 2px
     
     &__photo
-        margin-top: 16px;
+        margin-top: 12px
         background: #dedede
         min-height: 50px
 
     &__pic
         width: 100%
         display: block
+        border-radius: $card-border-radius       
+        cursor: pointer
+        cursor: -webkit-zoom-in
+        cursor: zoom-in
 
     &--overflowed
         .detail-message__closer,
         .detail-message__menu
             right: 16px;
+
+.blueimp-gallery
+    & > .close,
+    & > .next,
+    & > .prev
+        font-size:0;
+        border:0;
+        background: none;
+        padding:0;
+
+        &::before
+            position: absolute;
+            top:0;
+            bottom:0;
+            left:0;
+            right:0;
+            margin: auto;
+            font-family: "Material Icons";
+            font-size: 42px;
+            width:42px;
+            height: 42px;
+            line-height: 42px;
+
+    & > .close        
+        width: 72px;
+        height: 72px;
+
+        &::before
+            content: "close"
+
+    & > .next,
+    & > .prev
+        top:0;
+        bottom:0;
+        height:auto;
+        width: 50%;
+
+    & > .next
+        right:0;
+
+        &::before
+            content: "arrow_forward";
+            left: auto;
+            right: 15px;
+
+    & > .prev
+        left:0;
+        &::before
+            content: "arrow_back";
+            right: auto;
+            left: 15px;
 
 </style>
